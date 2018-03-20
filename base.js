b09530dc9fdaadb8099d4468f29c3b1f8dcc3dca
chrokh <christopher.okhravi@gmail.com>
2018-03-20 20:11:51 +0100

    Remove unused functions from base lib.

--- base.js
+++ base.js
@@ -1,9 +1,7 @@
 const adjust    = n => f => xs => mapi(x => i => i == n ? f(x) : x)(xs)
-const concat    = xs => ys => xs.concat(ys)
 const dropFirst = xs => xs.slice(1)
 const dropLast  = xs => xs.slice(0, xs.length - 1)
 const id        = x => x
-const join      = x => xs => xs.join(x)
 const k         = x => y => x
 const map       = f => xs => xs.map(f)
 const mapi      = f => xs => xs.map((x, i) => f(x)(i))
@@ -14,10 +12,9 @@ const pipe      = (...fns) => x => [...fns].reduce((acc, f) => f(acc), x)
 const prop      = k => o => o[k]
 const range     = n => m => Array.apply(null, Array(m - n)).map((_, i) => n + i)
 const rep       = c => n => map(k(c))(range(0)(n))
-const reverse   = xs => xs.reverse()
 const rnd       = min => max => Math.floor(Math.random() * max) + min
 const spec      = o => x => Object.keys(o)
   .map(k => objOf(k)(o[k](x)))
   .reduce((acc, o) => Object.assign(acc, o))
 
-module.exports = { adjust, concat, dropFirst, dropLast, id, join, k, map, mapi, merge, mod, objOf, pipe, prop, range, rep, reverse, rnd, spec }
+module.exports = { adjust, dropFirst, dropLast, id, k, map, merge, mod, objOf, pipe, prop, range, rep, rnd, spec }
