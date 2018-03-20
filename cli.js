9aaf33bf8c8354391d43189f2eb90f4d7a11b490
chrokh <christopher.okhravi@gmail.com>
2018-03-20 20:06:42 +0100

    Line up code.

--- cli.js
+++ cli.js
@@ -9,11 +9,11 @@ let State = Snake.initialState()
 // Matrix operations
 const Matrix = {
   make:      table => rep(rep('.')(table.cols))(table.rows),
-  set:       val => pos => adjust(pos.y)(adjust(pos.x)(k(val))),
+  set:       val   => pos => adjust(pos.y)(adjust(pos.x)(k(val))),
   addSnake:  state => pipe(...map(Matrix.set('X'))(state.snake)),
   addApple:  state => Matrix.set('o')(state.apple),
   addCrash:  state => state.snake.length == 0 ? map(map(k('#'))) : id,
-  toString:  xsxs => xsxs.map(xs => xs.join(' ')).join('\r\n'),
+  toString:  xsxs  => xsxs.map(xs => xs.join(' ')).join('\r\n'),
   fromState: state => pipe(
     Matrix.make,
     Matrix.addSnake(state),
