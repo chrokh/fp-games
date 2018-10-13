35ac911d43124babc3e0565cb488536ac4ee9ee6
antonstrand <jag@antonstrand.se>
2018-10-13 16:08:22 +0200

    issue #3

--- 001-snake/snake.js
+++ 001-snake/snake.js
@@ -9,14 +9,15 @@ const WEST  = { x:-1, y: 0 }
 
 // Point operations
 const pointEq = p1 => p2 => p1.x == p2.x && p1.y == p2.y
+const pointOp = p1 => p2 => p1.x == -p2.x && p1.y == -p2.y
 
 // Booleans
 const willEat   = state => pointEq(nextHead(state))(state.apple)
 const willCrash = state => state.snake.find(pointEq(nextHead(state)))
-const validMove = move => state =>
-  state.moves[0].x + move.x != 0 || state.moves[0].y + move.y != 0
+const validMove = move => state => !pointEq(move)(lastMove(state)) && !pointOp(move)(lastMove(state))
 
 // Next values based on state
+const lastMove = state => state.moves[state.moves.length - 1]
 const nextMoves = state => state.moves.length > 1 ? dropFirst(state.moves) : state.moves
 const nextApple = state => willEat(state) ? rndPos(state) : state.apple
 const nextHead  = state => state.snake.length == 0
